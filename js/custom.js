1f007a7a911b7f2758cd8b23944b78a866cb069f
Kjell <kjell@simplefox.de>
2016-11-18 12:37:09 +0100

    Einrichtung Mails senden

--- js/custom.js
+++ js/custom.js
@@ -26,7 +26,7 @@ PRBFrontEnd.prototype = {
         goalAmount: 150,
         currentAmount: 100,
         //textBefore: '$',
-        textAfter: ' € von 1000€ gesammelt'
+        textAfter: ' €'
     });
 
 
@@ -36,7 +36,38 @@ PRBFrontEnd.prototype = {
 
 			// Validierung hier
 
+			var vorname = jQuery('.prb_form #vorname').val();
+			var nachname = jQuery('.prb_form #nachname').val();
+			var tel = jQuery('.prb_form #tel').val();
+			var mail = jQuery('.prb_form #mail').val();
+			var bundesland = jQuery('.prb_form #bundesland').val();
+			var nonce = jQuery('.prb_form #mail_nonce').val();
 
+			var sendMails = jQuery.ajax({
+					url : Custom.ajaxurl,
+					type : 'post',
+					data : {
+						 action: 'send_prb_mails',
+						 vorname: vorname,
+						 nachname: nachname,
+						 bundesland: bundesland,
+						 tel: tel,
+						 mail: mail,
+						 nonce : nonce
+					},
+					beforeSend: function( xhr ) {
+						//jQuery(that).parent().append('<center><i style="font-size: 14px;color:#EA4E92;" class="fa fa-spinner fa-pulse fa-3x fa-fw"></i> wird bearbeitet</center>');
+						//jQuery('.edit, .close, .delete').remove();
+					},
+					error : function(error) {
+						console.log(error)
+						alert('Keine Berechtigung')
+					}
+			})
+
+			sendMails.done(function(response) {
+				console.log(response);
+			})
 
 
 			return false;
--- prbclasses/prb.prbreakfast.shortcodes.php
+++ prbclasses/prb.prbreakfast.shortcodes.php
@@ -26,7 +26,75 @@ class PRBshortCode {
     // Frontend GoalProgress
     add_shortcode( 'prbreakfast_donation_progress', array(&$this,'prbreakfast_donation_progress_function') );
 
+
+    add_action('wp_ajax_send_prb_mails', array( $this, 'send_prb_mails' ));
+    add_action('wp_ajax_nopriv_send_prb_mails', array( $this, 'send_prb_mails' ));
+
 	}
+
+  public function send_prb_mails()
+  {
+    if ( !wp_verify_nonce( $_REQUEST['nonce'], "send_prb_mails_nonce")) {
+      // TODO ERRORE zurücksenden
+      exit("No naughty business please");
+    }
+
+
+    $vorname =  $_REQUEST['vorname'];
+    $nachname =  $_REQUEST['nachname'];
+    $mail =  $_REQUEST['mail'];
+    $tel =  $_REQUEST['tel'];
+    $bundesland =  $_REQUEST['bundesland'];
+
+
+    echo $vorname;
+    echo $nachname;
+    echo $mail;
+    echo $tel;
+    echo $bundesland;
+
+
+    switch ($bundesland) {
+      case 'burgenland':
+        $mailTo = 'office@krebshilfe-bgld.at';
+        break;
+      case 'oberoesterreich':
+        $mailTo = 'office@krebshilfe-ooe.at';
+        break;
+      case 'tirol':
+        $mailTo = 'krebshilfe@i-med.ac.at';
+        break;
+      case 'kaernten':
+        $mailTo = 'office@krebshilfe-ktn.at';
+        break;
+      case 'salzburg':
+        $mailTo = 'office@krebshilfe-sbg.at';
+        break;
+      case 'voralberg':
+        $mailTo = 'office@krebshilfe-vbg.at';
+        break;
+      case 'niederoesterreich':
+        $mailTo = 'krebshilfe@krebshilfe-noe.at';
+        break;
+      case 'steiermark':
+        $mailTo = 'office@krebshilfe.at';
+        break;
+      case 'wien':
+        $mailTo = 'service@krebshilfe-wien.at';
+        break;
+      default:
+        $mailTo = 'service@krebshilfe.net';
+        break;
+    }
+    $koordinatorMail = 'service@krebshilfe.net';
+
+    // Testmail
+    $mailTo = 'kjell@simplefox.de';
+
+    
+
+    die();
+  }
 
   public function prbreakfast_donation_progress_function($atts)
   {
@@ -49,42 +117,24 @@ class PRBshortCode {
     ?>
     <div class="prb_form_container">
     	<form class="prb_form">
-    		<input class="prb_input" type="text" placeholder="Vorname" requiered>
-    		<input class="prb_input" type="text" placeholder="Nachname" requiered>
-    		<input class="prb_input" type="tel" placeholder="Handynummer" requiered>
-    		<input class="prb_input" type="email" placeholder="E-Mail" requiered>
+        <?php $nonce = wp_create_nonce('send_prb_mails_nonce'); ?>
+        <input type="hidden" id="mail_nonce" value="<?php echo $nonce;?>">
+    		<input class="prb_input" type="text" id="vorname" placeholder="Vorname" requiered>
+    		<input class="prb_input" type="text" id="nachname" placeholder="Nachname" requiered>
+    		<input class="prb_input" type="tel" id="tel" placeholder="Handynummer" requiered>
+    		<input class="prb_input" type="email" id="mail" placeholder="E-Mail" requiered>
     		<div class="clear"></div>
-    		<select class="prb_form_select">
-    			<option velue="waehle">
-    				Wähle dein Bundesland
-    			</option>
-    			<option velue="burgenland">
-    				Burgenland
-    			</option>
-    			<option velue="oberoesterreich">
-    				Oberösterreich
-    			</option>
-    			<option velue="tirol">
-    				Tirol
-    			</option>
-    			<option velue="kaernten">
-    				Kärnten
-    			</option>
-    			<option velue="salzburg">
-    				Salzburg
-    			</option>
-    			<option velue="voralberg">
-    				Voralberg
-    			</option>
-    			<option velue="niederoesterreich">
-    				Niederösterreich
-    			</option>
-    			<option velue="steiermark">
-    				Steiermark
-    			</option>
-    			<option velue="wien">
-    				Wien
-    			</option>
+    		<select class="prb_form_select" id="bundesland">
+    			<option velue="waehle">Wähle dein Bundesland</option>
+    			<option velue="burgenland">Burgenland</option>
+    			<option velue="oberoesterreich">Oberösterreich</option>
+    			<option velue="tirol">Tirol</option>
+    			<option velue="kaernten">Kärnten</option>
+    			<option velue="salzburg">Salzburg</option>
+    			<option velue="voralberg">Voralberg</option>
+    			<option velue="niederoesterreich">Niederösterreich</option>
+    			<option velue="steiermark">Steiermark</option>
+    			<option velue="wien">Wien</option>
     		</select>
 
     		<div class="prb_form_checkbox"><label for="prb_form_checkboxid"><input id="prb_form_checkboxid" type="checkbox" name="newsletter" value="newsletter">Ich darf vom Krebshilfeteam in meiner Nähe kontaktiert werden.<div class="prb_form_btn"></div></label></div>
